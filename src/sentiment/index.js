7748d3aa2cc6a0af23e61beb7965b5615130be58
John Armstrong <j.armstrong484@gmail.com>
2019-03-17 12:32:36 +1300

    Add function to analyse the sentiment of some text

---
+++ src/sentiment/index.js
@@ -0,0 +1,19 @@
+import language from '@google-cloud/language';
+import ramda from 'ramda';
+
+const languageClient = new language.LanguageServiceClient();
+
+
+export function analyseSentiment(text) {
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT'
+  };
+
+  return languageClient.analyzeSentiment({
+    document
+  }).catch((e) => {
+    console.log('ERROR: ', e);
+    return Promise.reject(e);
+  }); // This will sometimes reject when text is in a language google can not understand
+}
