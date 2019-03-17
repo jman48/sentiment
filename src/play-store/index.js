a7ce993269cbe25149b3aa7a80581c185a5602b4
John Armstrong <j.armstrong484@gmail.com>
2019-03-17 13:50:08 +1300

    Use debug module for logging

--- src/play-store/index.js
+++ src/play-store/index.js
@@ -1,6 +1,9 @@
 import playStore from "google-play-scraper";
+import makeDebug from 'debug';
 import { analyseSentiment } from "../sentiment";
 import { shouldNotify, notify } from "../notification";
+
+const debug = makeDebug('sentiment:playstore/index.js');
 
 function getReviews(appId, page = 0) {
   return playStore.reviews({
@@ -19,11 +22,12 @@ async function processReview(review) {
 export async function checkReviews(id) {
   try {
     const reviews = await getReviews(id);
+    debug(`Retrieved ${reviews.length} reviews`);
 
     for (let review of reviews) {
       await processReview(review);
     }
   } catch (error) {
-    console.log('Error: ', error);
+    debug(error);
   }
 }
