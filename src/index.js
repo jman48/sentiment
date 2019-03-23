fdf0a6b45516dfd66618d904d8bb050cf40a6985
John Armstrong <j.armstrong484@gmail.com>
2019-03-24 12:18:50 +1300

    Use dotenv to load env variables

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import 'babel-polyfill';
+import "babel-polyfill";
+import { config as loadEnv } from 'dotenv';
+import { checkReviews as checkPlayStoreReviews } from "./play-store";
+
+loadEnv();
 
 module.exports.hello = async (event, context) => {
+  await checkPlayStoreReviews('com.brainfm.app');
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: "Go Serverless v1.0! Your function executed successfully!",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
