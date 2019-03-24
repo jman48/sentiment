e701fc0d29e9aae5e5a97c776dbf944370851f62
John Armstrong <j.armstrong484@gmail.com>
2019-03-24 13:56:28 +1300

    Remove the serverless framework from this project as we do not want to use lambda anymore

--- src/index.js
+++ src/index.js
@@ -4,16 +4,10 @@ import { checkReviews as checkPlayStoreReviews } from "./play-store";
 
 loadEnv();
 
-module.exports.hello = async (event, context) => {
+async function run() {
   await checkPlayStoreReviews('com.brainfm.app');
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event
-    })
-  };
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+(async () => {
+  await run();
+})();
