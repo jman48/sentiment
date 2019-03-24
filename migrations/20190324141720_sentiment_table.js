b019651af933b779e4e996d928407b5173831a48
John Armstrong <j.armstrong484@gmail.com>
2019-03-24 14:30:08 +1300

    Create sentiment table to hold sentiment for each review

---
+++ migrations/20190324141720_sentiment_table.js
@@ -0,0 +1,21 @@
+const table = "sentiment";
+
+exports.up = knex => {
+  return knex.schema.createTable(table, t => {
+    t.increments("id")
+      .unsigned()
+      .primary()
+      .notNullable();
+    t.integer("reviewId")
+      .unsigned()
+      .index()
+      .references("reviews.id");
+    t.float("score");
+    t.float("magnitude");
+    t.string("language");
+  });
+};
+
+exports.down = knex => {
+  return knex.schema.dropTable(table);
+};
