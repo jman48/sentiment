bafaf7b4bba6024eeee7140d2dfa6f0ec298fda1
John Armstrong <j.armstrong484@gmail.com>
2019-04-07 11:30:48 +1200

    Use a promise so both functions will run at the same time (instead of running one after the other)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import "babel-polyfill";
-import { config as loadEnv } from 'dotenv';
+import { config as loadEnv } from "dotenv";
 import { checkReviews as checkPlayStoreReviews } from "./play-store";
+import { checkReviews as checkAppStoreReviews } from "./app-store";
 
 loadEnv();
 
 async function run() {
-  await checkPlayStoreReviews('com.brainfm.app');
+  await Promise.all([
+    await checkPlayStoreReviews("com.brainfm.app"),
+    await checkAppStoreReviews("1110684238")
+  ]);
 }
 
 (async () => {
