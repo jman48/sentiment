08db20f791df746bb2501febec3d296dfad460ce
John Armstrong <j.armstrong484@gmail.com>
2019-04-07 15:10:30 +1200

    Add a function to get all the new/ unprocessed reviews so we do not re-process reviews

--- src/review/index.js
+++ src/review/index.js
@@ -1,4 +1,5 @@
 import db from "../db";
+import { dropLast, findIndex, isNil } from "ramda";
 
 export async function updateLastReviewId(id, source) {
   const row = await db("source_last_id").where({ source });
@@ -14,4 +15,33 @@ export async function updateLastReviewId(id, source) {
       last_id: id
     });
   }
+}
+
+export async function getNewReviews(reviews, source) {
+  const [{ last_id }] = await db("source_last_id").where({
+    source
+  });
+
+  // No last_id so must be first run
+  if (isNil(last_id)) return reviews;
+
+  const [{ date }] = await db("reviews").where({ reviewId: last_id });
+
+  const lastReviewIndex = findIndex(review => {
+    return review.id === last_id;
+  }, reviews);
+
+  const lastReviewDate = new Date(date);
+  const latestReviewDate = new Date(reviews[0].date);
+
+  // Last review not found and these reviews are newer, so return all
+  if (lastReviewIndex === -1 && latestReviewDate >= lastReviewDate) {
+    return reviews;
+  }
+  // Last review not found and these reviews are older, so they must have already been processed
+  else if (lastReviewIndex === -1) return [];
+
+  // Found review, so return all the newer ones before it
+  const reviewsToDrop = reviews.length - lastReviewIndex;
+  return dropLast(reviewsToDrop, reviews);
 }
